c2b8ac649cd5190c4ceda6c9c310ec0335e10cf6
Joshua Castillo <castillo.josh7926@gmail.com>
2019-03-06 17:17:03 -0800

    started a left column

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,37 @@ import ReactDom from 'react-dom';
 var navCss = {
               borderTop:'1px solid #919191',
               borderBottom:'1px solid #919191',
+              margin: '0px',
               marginRight: '15%',
               marginLeft: '15%',
               marginTop: '10px',
               backgroundColor: '#d2d5db',
               height: '2em',
-              paddingTop: '5px'
-            };
+              width: '62%',
+              paddingTop: '5px',
+              display: 'block',
+              // float: 'right'
+};
+
+var leftColCss = {
+  borderLeft: '1px solid #919191',
+  borderRight: '1px solid #919191',
+  backgroundColor: '#d2d5db',
+  minHeight: '820px',
+  width: '20%',
+  float: 'left',
+  textAlign: 'center'
+};
+
+var rightColCss = {
+  borderLeft: '1px solid #919191',
+  borderRight: '1px solid #919191',
+  backgroundColor: '#d2d5db',
+  minHeight: '820px',
+  width: '20%',
+  float: 'right',
+  textAlign: 'center'
+};
 
 class App extends React.Component {
   constructor(props){
@@ -26,6 +50,13 @@ class App extends React.Component {
         <nav id="topBanner" style={navCss}>
           <h3 style={{verticalAlign: 'baseline', margin: '0px', textAlign: 'center'}}>Josh's Room</h3>
         </nav>
+
+        <div id="leftColumn" style={leftColCss}>
+          <p>hey there weenie</p>
+        </div>
+        <div id="rightColumn" style={rightColCss}>
+          <p>hey there weenie</p>
+        </div>
 
       </div>
     )
